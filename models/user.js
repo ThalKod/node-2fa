ca2f947ac3bd599c15cf21b23e50eaf1bb114d7f
ThalKod <tmarcelin@thal.tech>
2018-06-15 11:01:37 -0400

    add User model

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+});
+
+userSchema.plugin = (passportLocalMongoose);
+
+
+module.exports = mongoose.model("User", userSchema);
