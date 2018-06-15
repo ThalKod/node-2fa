e1193a7892f781732bf14d5e250fe245af1e289b
ThalKod <tmarcelin@thal.tech>
2018-06-15 10:54:30 -0400

    add mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
+// Configure app
 app.set("view engine", "ejs");
+
+// middlewares
 app.use(express.static(__dirname + "/public"));
 
+// configure mongoose and DB connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/node2fa", { reconnectTries: Number.MAX_VALUE }).then((db =>{
+    // boot
+    app.listen(5000, ()=>{
+        console.log("listenning on 5000");
+    });
+})).catch(dbErr =>{
+    console.log("Connection Error : ", dbErr.message);
+    process.exit(1);
+});
 
+
+
+// Routes
 app.get("/", (req, res)=>{
     res.redirect("/login");
 });
@@ -19,8 +36,6 @@ app.get("/login", (req, res)=>{
 });
 
 
-app.listen(5000, ()=>{
-    console.log("listenning on 5000");
-});
+
 
 
